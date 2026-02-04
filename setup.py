4baaad06f095c9e0f5b4e541fde5bcfe48e79053
Can Demircan <cdemircan@tutanota.com>
2026-02-04 14:02:55 +0100

    pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="groupBMC",
+    version="1.0.1",
+    py_modules=["groupBMC"],
+    install_requires=[
+        "numpy",
+        "scipy",
+    ],
+)
